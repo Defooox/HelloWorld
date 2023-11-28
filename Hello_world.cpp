d2872299290c6824db90a8e2dc663d2b39fac49b
ALIAKSANDR <97180975+Defooox@users.noreply.github.com>
2023-11-28 23:32:00 +0300

    no message

---
+++ Hello_world.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+#define HELLO_WORLD "hello world!" 
+#define HelloWorld greet 
+#define Hello_World +1 
+#define world malloc 
+#define HELLO sizeof 
+#define WORLD strlen 
+#define WORLD_HELLO strcpy 
+#define world_hello return 
+typedef char* hello_world;
+
+
+hello_world HelloWorld() {
+    hello_world hello_world;
+    hello_world = reinterpret_cast<char*>(world(HELLO(*hello_world) * WORLD(HELLO_WORLD) Hello_World));
+    WORLD_HELLO(hello_world, HELLO_WORLD);
+    world_hello hello_world;
+}
+
+int main() {
+    std::cout << HelloWorld();
+}
